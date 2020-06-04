26a4e2e696ff50f026008d59d202f8e2747fcdca
satriowputra <satriowputra@gmail.com>
2020-06-04 12:43:27 +0000

    improve cuda or cpu opt, add nn flexibility, add ar to load img, change predicting dev as per user input

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ import json
 
 import torch
 from torch import nn
-from torchvision import models
+from torchvision import transforms, models
 
 # Defining arguments or command line option
 parser = argparse.ArgumentParser(description='Arguments',)
@@ -25,49 +25,41 @@ results = parser.parse_args()
 with open(results.category_names, 'r') as f:
     cat_to_name = json.load(f)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+# Select processing unit as per user input
+if results.boolean_gpu:
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print("Predicting will be using CUDA as specified")
+    else:
+        device = torch.device("cpu")
+        print("Cuda device is not availabe. Predicting will continue using CPU")
+else:
+    device = torch.device("cpu")
+    print("Predicting will be using CPU")
+    
 # TODO: Write a function that loads a checkpoint and rebuilds the model
-if results.boolean_gpu == False:
+if device.type == 'cpu':
     checkpoint = torch.load(results.checkpoint_dir, map_location='cpu')
-elif results.boolean_gpu == True:
-    checkpoint = torch.load(results.checkpoint_dir, map_location='gpu')
+else:
+    checkpoint = torch.load(results.checkpoint_dir, map_location='cuda:0')
 
 if checkpoint['tl_arch'] == 'densenet161':
     model = models.densenet161(pretrained=True)
-    for param in model.parameters():
-        param.required_grad = False
-    
-    model.class_to_idx = checkpoint['class_to_idx']
-
-    classifier = nn.Sequential(nn.Linear(2208, 1000),
-                              nn.ReLU(),
-                              nn.Dropout(0.2),
-                              nn.Linear(1000,102),
-                              nn.LogSoftmax(dim=1))
-    model.classifier = classifier
-    model.load_state_dict(checkpoint['state_dict'])
-    model.eval()
 
 elif checkpoint['tl_arch'] == 'vgg16':
     model = models.vgg16(pretrained=True)
-    for param in model.parameters():
-        param.required_grad = False
-    
-    model.class_to_idx = checkpoint['class_to_idx']
 
-    classifier = nn.Sequential(nn.Linear(25088, 4096),
-                              nn.ReLU(),
-                              nn.Dropout(0.2),
-                              nn.Linear(4096,102),
-                              nn.LogSoftmax(dim=1))
-    model.classifier = classifier
-    model.load_state_dict(checkpoint['state_dict'])
-    model.eval()
-    
 else:
-    print("TL architecture is not correct")
+    print("TL architecture is not supported")
     
+for param in model.parameters():
+    param.required_grad = False
+
+model.classifier = checkpoint['classifier']
+model.class_to_idx = checkpoint['class_to_idx']
+model.load_state_dict(checkpoint['state_dict'])
+# model.to(device)
+model.eval()
 
 def process_image(image):
     ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
@@ -77,16 +69,27 @@ def process_image(image):
     # TODO: Process a PIL image for use in a PyTorch model
     from PIL import Image
     input_image = Image.open(image)
-    input_image = input_image.resize((256, 256))
+    
+    # Resizing image
+    width, height  = input_image.size
+    aspect_ratio = width / height
+    if width < height:
+        new_width = 256
+        new_height = int(new_width / aspect_ratio)
+    elif width > height:
+        new_height = 256
+        new_width = int(aspect_ratio*256)
+    else:
+        new_width = 256
+        new_height = 256
+    input_image = input_image.resize((new_width, new_height))
 
-    width = input_image.width
-    height = input_image.height
-
-#     (left, upper, right, lower) = (((width-224)/2), 224, 224, ((height-224)/2))
+    # Cropping image
+    width, height  = input_image.size
     left = (width-224)/2
-    upper = left + 224
     right = left + 224
     lower = (height-224)/2
+    upper = lower + 224
 
     input_image = input_image.crop((left, lower, right, upper))
 
@@ -106,15 +109,19 @@ def predict(image_path, model, topk=results.top_k):
     trained deep learning model.
     '''
     # TODO: Implement the code to predict the class from an image file
-    model.cpu()
+    model.to(device)
     img = process_image(image_path)
-    img = torch.from_numpy(img).type(torch.FloatTensor)
+    if device.type == 'cuda':
+        img = torch.from_numpy(img).type(torch.cuda.FloatTensor)
+    else:
+        img = torch.from_numpy(img).type(torch.FloatTensor)
+    img.to(device)
     img_input = img.unsqueeze(0)
     logps = model.forward(img_input)
     ps = torch.exp(logps)
     top_p, top_class = ps.topk(topk, dim=1)
-    top_p = top_p.detach().numpy().tolist()[0]
-    top_class = top_class.detach().numpy().tolist()[0]
+    top_p = top_p.cpu().detach().numpy().tolist()[0]
+    top_class = top_class.cpu().detach().numpy().tolist()[0]
     idx_to_class = {val: key for key, val in    
                                       model.class_to_idx.items()}
     top_class = [idx_to_class[label] for label in top_class]
